9829b467c837d8b5afb5da22013693b527be963a
Jake <jsutter449@west-mec.org>
2018-05-04 13:19:39 -0700

    please

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 const express = require('express');
 const app = express();
 const PORT = 8081;
-const OAuth = require('oauth').OAuth;
+var OAuth = require('OAuth').OAuth; // JAS - 1.24.18 - Grabbing the OAuth class from the OAuth module into a variable
 const config = require('../config.json');
 const bodyParser = require('body-parser');
 
@@ -53,11 +53,11 @@ var twitterCredentials = {
 
 
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
 
 
 
@@ -117,9 +117,6 @@ app.post('/api/getCurrentUser/:userId', (req, res) => {
   console.log("at " + req.body.access_token);
   let oauth_access_token_secret = req.body.oauth_access_token_secret;
   console.log("ats " + req.body.access_token_secret);
-  // let body = {
-  //   req.params.userId
-  // }
     oauth.get.call(oauth, url, access_token, oauth_access_token_secret, (err, data) => {
       if (err) {
         console.log(err);
@@ -183,113 +180,113 @@ app.get('/api/start', (req, res) => {
 });
 
 //
-app.get('/api/getBooks', (req, res) => {
-  var sql = "SELECT * FROM `books` ORDER BY bookTitle"
-  con.query(sql, (err, result, fields) => {
-    if (err) throw err;
-    res.send(result)
-  });
-});
-
-app.get('/api/getBooks/:bookNumbers', (req, res) => {
-  let bookNumbers = req.params.bookNumbers;
-  bookNumbers = bookNumbers.split("_");
-  let sql = "SELECT bookTitle, authorFName, authorLName FROM `books` WHERE bookID = '"
-  bookNumbers.forEach((bookNumber, i) => {
-    if (i == bookNumbers.length - 1) {
-      sql += bookNumber + "' ORDER BY bookTitle"
-      con.query(sql, (err, result, fields) => {
-        if (err) throw err;
-        // result = JSON.stringify(result);
-        console.log(result);
-        res.send(result)
-      });
-    } else {
-     sql += bookNumber + "' OR bookID = '"
-   }
-  })
-});
-
-app.get('/api/getUser/:userId', (req, res) => {
-  let userId = req.params.userId;
-  let sql = "SELECT memberLName, password FROM members WHERE memberID = " + userId
-  con.query(sql, (err, result, fields) => {
-    if (err) {
-      throw err;
-    }
-    res.send(result);
-  })
-})
-
-app.get('/api/checkoutBook/:userId/:bookId', (req, res) => {
-  let userId = req.params.userId;
-  let bookId = req.params.bookId;
-  let todayDate = new Date();
-  let rentalDate = todayDate.getFullYear() + "-" + (todayDate.getMonth() + 1) + "-" + todayDate.getDate();
-  let dueDate = new Date(Date.now() + 12096e5);
-  dueDate = dueDate.getFullYear() + "-" + (dueDate.getMonth() + 1) + "-" + dueDate.getDate();
-
-  let sql = "INSERT INTO rentals (memberID, bookID, rentalDate, dueDate, returned) VALUES ('" + userId + "', '" + bookId + "', '" + rentalDate + "', '" + dueDate + "', 'F')"
-  con.query(sql, (err, result) => {
-    if (err) {
-      console.log("Error Inserting new rental");
-      throw err;
-    }
-    let sql2 = "UPDATE books SET available = 'F' WHERE bookID = " + bookId
-    con.query(sql2, (err, result) => {
-      if (err) {
-        console.log("Error Updating Book");
-        throw err;
-      }
-      res.send(result)
-    })
-  })
-})
-
-app.get('/api/reserveBook/:userId/:bookId', (req, res) => {
-  let userId = req.params.userId;
-  let bookId = req.params.bookId;
-  let todayDate = new Date();
-  let rentalDate = todayDate.getFullYear() + "-" + (todayDate.getMonth() + 1) + "-" + todayDate.getDate();
-  let dueDate = new Date(Date.now() + 12096e5);
-  dueDate = dueDate.getFullYear() + "-" + (dueDate.getMonth() + 1) + "-" + dueDate.getDate();
-
-  let sql = "INSERT INTO reservations (memberID, bookID, active) VALUES ('" + userId + "', '" + bookId + "', 'T')"
-  con.query(sql, (err, result) => {
-    if (err) {
-      console.log("Error Inserting new rental");
-      throw err;
-    }
-    res.send(result)
-
-  })
-})
-
-app.get('/api/userInformation/:userId', (req, res) => {
-  let userId = req.params.userId;
-  let userInfo = {}
-
-  let sql = "SELECT memberFName FROM members WHERE memberID = " + userId
-  con.query(sql, (err, result, fields) => {
-    if (err) {
-      console.log("Error getting user's name");
-      throw err;
-    }
-    userInfo.name = result[0].memberFName
-    sql = "SELECT bookID, dueDate FROM rentals WHERE memberID = " + userId + " AND returned = 'F'"
-    con.query(sql, (err, result, fields) => {
-      userInfo.books = result;
-      sql = "SELECT bookID FROM reservations WHERE memberID = " + userId + " AND active = 'T'"
-      con.query(sql, (err, result, fields) => {
-        userInfo.reservations = result;
-        res.send(userInfo);
-      })
-    })
-
-    // userInfo.name = result
-
-  })
-})
+// app.get('/api/getBooks', (req, res) => {
+//   var sql = "SELECT * FROM `books` ORDER BY bookTitle"
+//   con.query(sql, (err, result, fields) => {
+//     if (err) throw err;
+//     res.send(result)
+//   });
+// });
+//
+// app.get('/api/getBooks/:bookNumbers', (req, res) => {
+//   let bookNumbers = req.params.bookNumbers;
+//   bookNumbers = bookNumbers.split("_");
+//   let sql = "SELECT bookTitle, authorFName, authorLName FROM `books` WHERE bookID = '"
+//   bookNumbers.forEach((bookNumber, i) => {
+//     if (i == bookNumbers.length - 1) {
+//       sql += bookNumber + "' ORDER BY bookTitle"
+//       con.query(sql, (err, result, fields) => {
+//         if (err) throw err;
+//         // result = JSON.stringify(result);
+//         console.log(result);
+//         res.send(result)
+//       });
+//     } else {
+//      sql += bookNumber + "' OR bookID = '"
+//    }
+//   })
+// });
+//
+// app.get('/api/getUser/:userId', (req, res) => {
+//   let userId = req.params.userId;
+//   let sql = "SELECT memberLName, password FROM members WHERE memberID = " + userId
+//   con.query(sql, (err, result, fields) => {
+//     if (err) {
+//       throw err;
+//     }
+//     res.send(result);
+//   })
+// })
+//
+// app.get('/api/checkoutBook/:userId/:bookId', (req, res) => {
+//   let userId = req.params.userId;
+//   let bookId = req.params.bookId;
+//   let todayDate = new Date();
+//   let rentalDate = todayDate.getFullYear() + "-" + (todayDate.getMonth() + 1) + "-" + todayDate.getDate();
+//   let dueDate = new Date(Date.now() + 12096e5);
+//   dueDate = dueDate.getFullYear() + "-" + (dueDate.getMonth() + 1) + "-" + dueDate.getDate();
+//
+//   let sql = "INSERT INTO rentals (memberID, bookID, rentalDate, dueDate, returned) VALUES ('" + userId + "', '" + bookId + "', '" + rentalDate + "', '" + dueDate + "', 'F')"
+//   con.query(sql, (err, result) => {
+//     if (err) {
+//       console.log("Error Inserting new rental");
+//       throw err;
+//     }
+//     let sql2 = "UPDATE books SET available = 'F' WHERE bookID = " + bookId
+//     con.query(sql2, (err, result) => {
+//       if (err) {
+//         console.log("Error Updating Book");
+//         throw err;
+//       }
+//       res.send(result)
+//     })
+//   })
+// })
+//
+// app.get('/api/reserveBook/:userId/:bookId', (req, res) => {
+//   let userId = req.params.userId;
+//   let bookId = req.params.bookId;
+//   let todayDate = new Date();
+//   let rentalDate = todayDate.getFullYear() + "-" + (todayDate.getMonth() + 1) + "-" + todayDate.getDate();
+//   let dueDate = new Date(Date.now() + 12096e5);
+//   dueDate = dueDate.getFullYear() + "-" + (dueDate.getMonth() + 1) + "-" + dueDate.getDate();
+//
+//   let sql = "INSERT INTO reservations (memberID, bookID, active) VALUES ('" + userId + "', '" + bookId + "', 'T')"
+//   con.query(sql, (err, result) => {
+//     if (err) {
+//       console.log("Error Inserting new rental");
+//       throw err;
+//     }
+//     res.send(result)
+//
+//   })
+// })
+//
+// app.get('/api/userInformation/:userId', (req, res) => {
+//   let userId = req.params.userId;
+//   let userInfo = {}
+//
+//   let sql = "SELECT memberFName FROM members WHERE memberID = " + userId
+//   con.query(sql, (err, result, fields) => {
+//     if (err) {
+//       console.log("Error getting user's name");
+//       throw err;
+//     }
+//     userInfo.name = result[0].memberFName
+//     sql = "SELECT bookID, dueDate FROM rentals WHERE memberID = " + userId + " AND returned = 'F'"
+//     con.query(sql, (err, result, fields) => {
+//       userInfo.books = result;
+//       sql = "SELECT bookID FROM reservations WHERE memberID = " + userId + " AND active = 'T'"
+//       con.query(sql, (err, result, fields) => {
+//         userInfo.reservations = result;
+//         res.send(userInfo);
+//       })
+//     })
+//
+//     // userInfo.name = result
+//
+//   })
+// })
 
 
 
@@ -386,4 +383,6 @@ clearInterval(calcDatesInterval)
 
 app.listen(PORT, ()=> {
   console.log('Server listening on port:' + PORT);
+  console.log("OAuth callback hostname: " + url.parse(config.oauth_callback).hostname); // JAS - 1.23.18 - debug code to identify the oauth_callback's hostname
+  console.log("OAuth callback path: " + url.parse(config.oauth_callback).path); // JAS - 1.23.18 - debug code to identify the oauth_callback's path name
 })
